d88cd691d25fef516e6e1b4e039d883b89ce7108
madruga665 <madruga665@hotmail.com>
2026-02-22 21:37:21 -0300

    chore: add event location property

--- sql/init.sql
+++ sql/init.sql
@@ -2,9 +2,10 @@ CREATE TABLE IF NOT EXISTS events (
     id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     name VARCHAR(255) NOT NULL,
+    location VARCHAR(255),
     flyer TEXT
 );
 
-INSERT INTO events (date, name, flyer) VALUES
-('2025-12-08', 'Aniversário Moto Club Dragões', 'https://aws.bucket.com/foto/1'),
-(CURRENT_DATE, 'Aniversário Moto Club Piratas', 'https://aws.bucket.com/foto/2');
+INSERT INTO events (date, name, location, flyer) VALUES
+('2025-12-08', 'Aniversário Moto Club Dragões', 'Sede Moto Club Dragões', 'https://aws.bucket.com/foto/1'),
+(CURRENT_DATE, 'Aniversário Moto Club Piratas', 'Sede Moto Club Piratas', 'https://aws.bucket.com/foto/2');
